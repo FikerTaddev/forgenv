d10fe46259ad5c33ad13c73eb350f68f84c40d20
fikertaddev <fikertad.dev@hotmail.com>
2026-04-03 10:56:37 +0300

    feat: support for multiple env load enabled

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,11 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import fs from "fs";
 
-export function loadEnv() {
-     dotenv.config()
-      return process.env
+export function loadEnv(files: string[] = [".env"]) {
+    for (const file of files) {
+        if (fs.existsSync(file)) {
+            dotenv.config({ path: file });
+        }
+    }
+    return process.env;
 }
